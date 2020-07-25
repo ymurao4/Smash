4e12869b877e853b1bdc7f2f415c429b3d709ea2
Murao <yoshinobu612@icloud.com>
2020-07-25 15:03:36 +0900

    create AnalysisView

---
+++ Smash/ViewModels/AnalysisViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  AnalysisViewModel.swift
+//  Smash
+//
+//  Created by 村尾慶伸 on 2020/07/25.
+//  Copyright © 2020 村尾慶伸. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class AnalysisViewModel: ObservableObject {
+
+    
+
+}
--- Smash/Views/AnalysisView.swift
+++ Smash/Views/AnalysisView.swift
@@ -12,6 +12,7 @@ struct AnalysisView: View {
 
     @State var selectedIndex: Int = 0
     private let pickerName: [String] = ["メイン", "自分", "相手", "ステージ"]
+    private let sortedName: [String] = ["キャラ", "試合", "勝ち", "負け", "勝率"]
 
     var body: some View {
         NavigationView {
@@ -23,7 +24,33 @@ struct AnalysisView: View {
                     }
                 }
                 .pickerStyle(SegmentedPickerStyle())
-                
+                HStack {
+                    ForEach(sortedName, id: \.self) { name in
+                        Text(name)
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                .font(.subheadline)
+                .padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 10))
+                List {
+                    ForEach(0..<S.fightersArray.count) { index in
+                        HStack() {
+                            FighterPDF(name: S.fightersArray[index][1])
+                                .frame(width: 25, height: 25)
+                                .frame(maxWidth: .infinity)
+                            Text("5")
+                                .frame(maxWidth: .infinity)
+                            Text("3")
+                                .frame(maxWidth: .infinity)
+                            Text("2")
+                                .frame(maxWidth: .infinity)
+                            Text("60.0%")
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.2)
+                                .frame(maxWidth: .infinity)
+                        }
+                    }
+                }
             }
             .padding()
             .navigationBarTitle("分析")

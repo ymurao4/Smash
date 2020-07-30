eabc48ab8ecfda68eb103d7785964d3a1e4e0e34
Murao <yoshinobu612@icloud.com>
2020-07-30 11:31:06 +0900

    change displaying Icon

--- Smash/Views/NoteViews/NoteDetailView.swift
+++ Smash/Views/NoteViews/NoteDetailView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import WaterfallGrid
+import ExytePopupView
 
 struct NoteDetailView: View {
 
@@ -15,45 +16,34 @@ struct NoteDetailView: View {
 
     @State var isBeginEditing: Bool = false
     @State var isIconSetting: Bool = false
+    @State var keyboardHeight: CGFloat = 0
 
     var onCommit: (Note) -> (Void) = { _ in }
 
     var body: some View {
-        ZStack(alignment: .trailing) {
+        ZStack(alignment: .bottomTrailing) {
             // 入力中ではなく、isIconSettingがtrueの時表示
             MultilineTextField(text: $noteCellVM.note.text, isBeginEditing: $isBeginEditing)
-            // アイコン設定
-            if isBeginEditing {
-                if isIconSetting {
-                    HStack {
-                        WaterfallGrid(S.fightersArray, id: \.self) { fighter in
-                            Button(action: {
-                                self.noteCellVM.note.fighterName = fighter[1]
-                                self.isIconSetting = false
-                            }) {
-                                FighterPDF(name: fighter[1])
-                                    .frame(width: 30, height: 30)
-                            }
-                        }
-                        .gridStyle(columns: 8, spacing: 10, padding: EdgeInsets(top: 50, leading: 0, bottom: 0, trailing: 0), animation: .easeInOut(duration: 1))
-                        .scrollOptions(direction: .vertical, showsIndicators: false)
-                    }
-                    .offset(y: -40)
+            // アイコン設
+            Button(action: {
+                self.isIconSetting.toggle()
+                self.isBeginEditing = false
+                UIApplication.shared.endEditing()
+            }) {
+                if self.noteCellVM.note.fighterName != "" && self.noteCellVM.note.fighterName != nil {
+                    FighterPDF(name: self.noteCellVM.note.fighterName!)
                 } else {
-                    Button(action: {
-                        self.isIconSetting.toggle()
-                    }) {
-                        Image(systemName: "plus")
-                            .resizable()
-                            .frame(width: 15, height: 15)
-                    }
-                    .frame(width: 40, height: 40)
-                    .background(Color(UIColor.systemGray2))
-                    .foregroundColor(Color.white)
-                    .cornerRadius(20)
-                    .offset(y: 40)
+                    Image(systemName: "plus")
+                        .resizable()
+                        .frame(width: 15, height: 15)
                 }
             }
+            .frame(width: 45, height: 45)
+            .background(Color(UIColor.systemGray2))
+            .foregroundColor(Color.white)
+            .cornerRadius(22.5)
+            .animation(.spring())
+            .offset(y: -self.keyboardHeight)
         }
         .padding(.horizontal, 10)
         .navigationBarTitle(Text(""), displayMode: .inline)
@@ -68,37 +58,57 @@ struct NoteDetailView: View {
                         Text("Done")
                     }
                 }
-//                else {
-//                    if self.noteCellVM.note.fighterName != nil && self.noteCellVM.note.fighterName != "" {
-//                        Button(action: {
-//                            self.noteCellVM.note.fighterName = ""
-//                        }) {
-//                            Image(systemName: "multiply")
-//                                .resizable()
-//                                .frame(width: 16, height: 16)
-//                        }
-//                        .padding(.trailing, 10)
-//                    }
-//                    Button(action: {
-//                        self.isIconSetting.toggle()
-//                    }) {
-//                        if self.noteCellVM.note.fighterName == nil || self.noteCellVM.note.fighterName == "" {
-//                            Image(systemName: "person.crop.circle.badge.plus")
-//                                .resizable()
-//                                .frame(width: 23, height: 23)
-//                        } else {
-//                            if self.noteCellVM.note.fighterName != "" {
-//                                FighterPDF(name: self.noteCellVM.note.fighterName!)
-//                                    .frame(width: 25, height: 25)
-//                            }
-//                        }
-//                    }
-//                }
             }
         )
             .onDisappear {
                 self.onCommit(self.noteCellVM.note)
         }
+        .popup(isPresented: $isIconSetting, type: .toast, closeOnTap: true, closeOnTapOutside: true) {
+            SelectFighterIcon(noteCellVM: self.noteCellVM, isIconSetting: self.$isIconSetting)
+                .frame(maxHeight: UIScreen.main.bounds.height / 2)
+                .background(Color(UIColor.systemGray2))
+                .cornerRadius(30)
+                .animation(.spring())
+        }
+        .onAppear {
+
+            // keyboardの高さを取得
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { (noti) in
+                let value = noti.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+                let height = value.height
+
+                self.keyboardHeight = height
+            }
+
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { (noti) in
+
+                self.keyboardHeight = 0
+            }
+
+        }
     }
 }
 
+
+struct SelectFighterIcon: View {
+
+    @ObservedObject var noteCellVM: NoteCellViewModel
+    @Binding var isIconSetting: Bool
+
+    var body: some View {
+        HStack(alignment: .center) {
+            WaterfallGrid(S.fightersArray, id: \.self) { fighter in
+                Button(action: {
+                    self.noteCellVM.note.fighterName = fighter[1]
+                    self.isIconSetting = false
+                }) {
+                    FighterPDF(name: fighter[1])
+                        .frame(width: 30, height: 30)
+                }
+            }
+            .gridStyle(columns: 8, spacing: 10, padding: EdgeInsets(top: 10, leading: 10, bottom: 5, trailing: 10))
+            .scrollOptions(direction: .vertical, showsIndicators: false)
+        }
+    }
+
+}
